ddf11e5d47b2802679b7dc7d12d8af7e2d3b035f
TatianaKoba <lila-lilu@mail.ru>
2022-09-14 22:24:37 +0300

    master
    1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Simple_code__Globalisation_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1.9";
+            NumberFormatInfo NumberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+
+            };
+            double a = Convert.ToDouble(str, NumberFormatInfo);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
