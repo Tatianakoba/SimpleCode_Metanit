5c370d1133f3b824cf0046514a3d4e53ce26b004
TatianaKoba <lila-lilu@mail.ru>
2022-09-21 16:42:46 +0300

    21/09/2022 (Case)

---
+++ Constructions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructions
+{//break, goto case, return, throw
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите Y или N");
+            string selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "Y":
+                    Console.WriteLine("Вы нажали клавишу Y");
+                    break;
+                case "N":
+                    Console.WriteLine("Вы нажали клавишу N");
+                    break ;
+            }
+
+
+        }
+    }
+}
