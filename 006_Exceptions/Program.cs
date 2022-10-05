6c9fa05d73748ecab84a6be2ccca4985f4ed59e0
TatianaKoba <lila-lilu@mail.ru>
2022-10-05 19:59:18 +0300

    Ex, Refs

---
+++ 006_Exceptions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try... catch... finally
+
+            int[] nums = new int[4];
+            try
+            {
+                Console.WriteLine("Введите строку:");
+                string message = Console.ReadLine();
+                if (message.Length>6)
+                {
+                    throw new Exception("Длина строки больше 6 симовлов");
+                }
+                nums[5] = 10;
+                int x = 0;
+                int y = 9 / x;
+                Console.WriteLine("End of try block");
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Error occured: {ex.Message}");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error occured: {ex.Message}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"General error occured: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 007_Variables_links/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Variables_links
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ref
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            ref int nRef = ref Find(numbers, 4);
+            Console.WriteLine(numbers[3]);
+            nRef = 45;
+            Console.WriteLine(numbers[3]);
+            int x = 5;
+            ref int xRef = ref x;
+            Console.WriteLine($"x={x}");
+            Console.WriteLine($"xRef={xRef}");
+            xRef = 125;
+            Console.WriteLine($"x={x}");
+
+            Console.ReadKey();
+
+        }
+        static ref int Find(int[] numbers, int number)
+        {
+            for(int i= 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    return ref numbers[i];
+                }
+                
+            }
+            throw new Exception("число не найдено");
+        }
+    }
+}
