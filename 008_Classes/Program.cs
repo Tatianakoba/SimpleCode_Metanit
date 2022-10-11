ee6ca0c27cb138832e7c7b1ae4d95dd79ee10674
TatianaKoba <lila-lilu@mail.ru>
2022-10-12 00:15:10 +0300

    Classes

---
+++ 008_Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User tom = new User("Tom",23);
+            //tom.name = "Tom";
+            //tom.age = 22;
+            tom.Info();
+            Console.ReadKey();
+        }
+
+    }
+    class User
+    {
+        public string name;
+        public int age;
+        public void Info()
+        {
+            Console.WriteLine($"{name} - {age}");
+        }
+        public User(string name)
+        {
+            this.name = name;
+        }
+        public User(string n, int a) : this(n)
+        {
+            
+            age = a;
+        }
+    }
+}
