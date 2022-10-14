86d7e1e3ab65c40b0c40dda8aa04faa05b5dbb76
TatianaKoba <lila-lilu@mail.ru>
2022-10-14 23:43:07 +0300

    static classes

---
+++ 012_Methods(overloading)/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _012_Methods_overloading_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.Add(1,2);
+            calc.Add(1,2,3);
+            calc.Add(1.2,3.0);
+            calc.Add(1, 2, 3, 4);
+            Console.ReadKey();
+
+        }
+    }
+    class Calculator
+    {
+        public void Add (int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine($"result = {result}") ;
+        }
+        public void Add (int x, int y, int z)
+        {
+            int result = x + y+z ;
+            Console.WriteLine($"result = {result}");
+        }
+
+        public void Add(double x, double y)
+        {
+            double result = x + y;
+            Console.WriteLine($"result = {result}");
+        }
+        public int Add(int x, int y, int z, int t)
+        {
+            int result = x + y+x+t;
+            
+            Console.WriteLine($"result = {result}");
+            return x + y;
+        }
+    }
+}
---
+++ 013_Static_Classes/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_Static_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Account acc1 = new Account(1000, 6);
+            //Account acc2 = new Account(900, 5);
+            //Account.MinSum = 200;
+            //Account.GetSum(1000, 10, 5);
+            //acc2.MinSum = 200;
+            //Console.WriteLine(acc1.GetSum(1000,10,5));
+            //Console.WriteLine(acc2.GetSum(1000,10,5));
+            User user1 = new User(); //при создании первого пользователя был выполнен статический конструктор
+            User user2 = new User();
+            User user3 = new User();
+            User.Display();
+            Console.WriteLine($"User1 Id = {user1.Id}");
+            Console.WriteLine($"User3 Id = {user3.Id}");
+
+            Console.ReadKey();
+        }
+    }
+    class User
+    {
+        private static int counter = 0;
+        public int Id { get; set; }
+        static User() //статический конструктор
+        {
+            Console.WriteLine("First User is created");
+        }
+        public User()
+        {
+            Id = ++counter;
+        }
+        public static void Display()
+        {
+            Console.WriteLine($"Количество зарегистрированных пользователей {counter}");
+        }
+    }
+    class Account
+    { 
+        //Минимально допустимая сумма для всех счетов
+        private static decimal minSum = 100;
+        public Account(decimal sum, decimal rate)
+        {
+            if (sum < minSum) throw new Exception("Недопустимая сумма!");
+            Sum = sum; Rate = rate;
+        }
+        public static decimal MinSum
+        {
+            get { return minSum; }
+            set {if (value>0) minSum=value ; } 
+        }
+        public decimal Sum { get; private set; }//сумма на счете
+        public decimal Rate { get; private set; }//процентная ставка
+
+        //подсчет суммы на счете через определенный период по определенной процентной ставке
+        public static decimal GetSum(decimal sum, decimal rate, int period)
+        {
+            decimal result = sum;
+            for (int i= 0; i < period; i++)
+                result = result+result * rate/100;
+            return result;
+
+        }
+    }
+}
