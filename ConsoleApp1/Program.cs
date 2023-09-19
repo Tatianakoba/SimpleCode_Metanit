50e7993c8de1f941cc48082951fbc379935b5b52
TatianaKoba <lila-lilu@mail.ru>
2023-09-19 23:24:19 +0300

    CodeBlog(1)

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string a = "Wake up, Neo...";
+            foreach (char ch in a)
+            {
+                Console.Write(ch);
+                Thread.Sleep(50);
+            }
+            Thread.Sleep(5000);
+            Console.Clear();
+            String b = "The Matrix has you...";
+            foreach (char ch in b)
+            {
+                Console.Write(ch);
+                Thread.Sleep(50);
+            }
+            
+            Console.ReadLine();
+            Console.Clear();
+            String c = "Follow the white rabbit";
+            foreach (char ch in c)
+            {
+                Console.Write(ch);
+                Thread.Sleep(50);
+            }
+            
+            Console.ReadLine();
+            Console.Clear();
+            String d = "Knok, knok, Neo";
+            foreach (char ch in d)
+            {
+                Console.Write(ch);
+                Thread.Sleep(50);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
