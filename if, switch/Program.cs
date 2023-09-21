94abf1c7473a8df55b1c3560d82fd03bd01c99d7
TatianaKoba <lila-lilu@mail.ru>
2023-09-21 23:26:15 +0300

    if,switch(6)

---
+++ if, switch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace if__switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите 3 числа:");
+            //var variables = Console.ReadLine().Split(' ');
+            //int a = int.Parse(variables[0]);
+            //int b = int.Parse(variables[1]);
+            //int c = int.Parse(variables[2]);
+            //if ((a>b)&&(a>c)) 
+            //{
+            //    Console.WriteLine("Наибольшее {0}",a);
+            //}
+            //else if ((b>c)&&(b>a))
+            //{
+            //    Console.WriteLine("Наибольшее {0}", b);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Наибольшее {0}", c);
+            //}
+
+            Console.WriteLine("Введите  число:");
+            int x = int.Parse(Console.ReadLine());
+            int y = x % 2;
+            switch (y)
+            {
+                case 0:
+                    y = 0;
+                    Console.WriteLine("Четное");
+
+                    break;
+                case 1:
+                    Console.WriteLine("Нечетное");
+                    break;
+
+            }
+            var s = x < 100 ?  "да" : "нет";
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ if, switch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("if, switch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("if, switch")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6429a3fb-4ac2-474f-85d2-9768d2e64911")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
