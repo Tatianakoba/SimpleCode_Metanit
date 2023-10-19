9460333f0d514fec6a823903a5b4ff987e06df41
TatianaKoba <lila-lilu@mail.ru>
2023-10-19 19:50:16 +0300

    Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        public static int Fibonacci(int value)
+        {
+            if (value <= 1)
+            {
+                return value =1;
+
+            }
+            
+            else
+            {
+                return value = Fibonacci(value - 2) + Fibonacci(value - 1);
+            }            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(9));
+            Console.ReadKey();
+        }
+       
+    }
+}
